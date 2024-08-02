71eb00a646cd2f6e28f678317049df1403796f37
Syed Ahmed <fullyflaredd@gmail.com>
2024-08-02 02:18:22 -0700

    Initial commit

---
+++ initsupabase.js
@@ -0,0 +1,3 @@
+const supabaseUrl = 'https://thpphchqfvljxfqofzsc.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRocHBoY2hxZnZsanhmcW9menNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE4NTc3MjMsImV4cCI6MjAzNzQzMzcyM30.-T35epDMvaZ7J9_qMbP7woAlUWwbvxPTJt7T0cVorEA';
+window.supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
