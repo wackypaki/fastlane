9c84e36a3fb7a1bb6a43f6e0d40558a91f1c3f36
Syed Ahmed <fullyflaredd@gmail.com>
2024-08-02 22:18:45 -0700

    ok dude

--- public/initsupabase.js
+++ public/initsupabase.js
@@ -1,5 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://thpphchqfvljxfqofzsc.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRocHBoY2hxZnZsanhmcW9menNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE4NTc3MjMsImV4cCI6MjAzNzQzMzcyM30.-T35epDMvaZ7J9_qMbP7woAlUWwbvxPTJt7T0cVorEA';
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const SUPABASE_URL = 'https://thpphchqfvljxfqofzsc.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRocHBoY2hxZnZsanhmcW9menNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE4NTc3MjMsImV4cCI6MjAzNzQzMzcyM30.-T35epDMvaZ7J9_qMbP7woAlUWwbvxPTJt7T0cVorEA';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
